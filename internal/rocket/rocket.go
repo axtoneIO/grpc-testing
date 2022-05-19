c69a254e57df1b56454bd88c07b9f78bb12b96aa
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2022-05-18 23:06:10 -0500

    first method implementation, GetRocketById

--- internal/rocket/rocket.go
+++ internal/rocket/rocket.go
@@ -1,7 +1,9 @@
 package rocket
 
+import "context"
+
 // Rocket - should contain the definition of our
-// rocket 
+// rocket
 type Rocket struct {
 	ID 		string
 	Name 	string
@@ -9,13 +11,34 @@ type Rocket struct {
 	Flights int
 }
 
+// store defines the interface we expect
+// our database implementation to follow
+type Store interface{
+	GetRocketById(id string)(Rocket,error)
+	InsertRocket(rkt Rocket)(Rocket,error)
+	DeleteRocket(id string)error
+}
+
 // Service - our rocket service, responsible for
 // updating the rocket inventory
 type Service struct {
-
+	Store Store
 }
 
 // New - returns a new instance of out rocket service 
-func New() Service {
-	return Service{}
+func New(store Store) Service {
+	return Service{
+		Store: store,
+	}
+}
+
+// GetRocketById - retrieves a rocket by id from the store
+func (s Service) GetRocketById (ctx context.Context, id string) (Rocket,error) {
+	rkt, err := s.Store.GetRocketById(id)
+
+	if err != nil{
+		return Rocket{},nil
+	}
+
+	return rkt,nil
 }
