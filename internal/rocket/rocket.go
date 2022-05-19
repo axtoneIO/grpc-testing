f791aded17c2694301ad44d32682a84e920986e6
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2022-05-19 00:11:24 -0500

    generated code with mockgen

--- internal/rocket/rocket.go
+++ internal/rocket/rocket.go
@@ -1,3 +1,4 @@
+//go:generate mockgen -destination=rocket_mocks_test.go -package=rocket github.com/axtoneIO/grpc-testing/internal/rocket Store
 package rocket
 
 import "context"
@@ -5,17 +6,17 @@ import "context"
 // Rocket - should contain the definition of our
 // rocket
 type Rocket struct {
-	ID 		string
-	Name 	string
-	Type 	string
+	ID      string
+	Name    string
+	Type    string
 	Flights int
 }
 
 // store defines the interface we expect
 // our database implementation to follow
-type Store interface{
-	GetRocketById(id string)(Rocket,error)
-	InsertRocket(rkt Rocket)(Rocket,error)
+type Store interface {
+	GetRocketById(id string) (Rocket, error)
+	InsertRocket(rkt Rocket) (Rocket, error)
 	DeleteRocket(id string) error
 }
 
@@ -25,7 +26,7 @@ type Service struct {
 	Store Store
 }
 
-// New - returns a new instance of out rocket service 
+// New - returns a new instance of out rocket service
 func New(store Store) Service {
 	return Service{
 		Store: store,
@@ -33,18 +34,18 @@ func New(store Store) Service {
 }
 
 // GetRocketById - retrieves a rocket by id from the store
-func (s Service) GetRocketById (ctx context.Context, id string) (Rocket,error) {
+func (s Service) GetRocketById(ctx context.Context, id string) (Rocket, error) {
 	rkt, err := s.Store.GetRocketById(id)
 
-	if err != nil{
+	if err != nil {
 		return Rocket{}, err
 	}
 
-	return rkt,nil
+	return rkt, nil
 }
 
 // InsertRocket - inserts a new rocket into the store
-func (s Service) InsertRocket (ctx context.Context, rkt Rocket) (Rocket,error) {
+func (s Service) InsertRocket(ctx context.Context, rkt Rocket) (Rocket, error) {
 	rkt, err := s.Store.InsertRocket(rkt)
 
 	if err != nil {
@@ -55,7 +56,7 @@ func (s Service) InsertRocket (ctx context.Context, rkt Rocket) (Rocket,error) {
 }
 
 // DeleteRocket - deletes a rocket by id from the store
-func (s Service) DeleteRocket (id string) error {
+func (s Service) DeleteRocket(id string) error {
 	err := s.Store.DeleteRocket(id)
 
 	if err != nil {
