94cd40da0572abd713ffb215915619a4a9a64914
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2022-05-18 23:35:12 -0500

    Rocket services added.

--- internal/rocket/rocket.go
+++ internal/rocket/rocket.go
@@ -16,7 +16,7 @@ type Rocket struct {
 type Store interface{
 	GetRocketById(id string)(Rocket,error)
 	InsertRocket(rkt Rocket)(Rocket,error)
-	DeleteRocket(id string)error
+	DeleteRocket(id string) error
 }
 
 // Service - our rocket service, responsible for
@@ -37,8 +37,30 @@ func (s Service) GetRocketById (ctx context.Context, id string) (Rocket,error) {
 	rkt, err := s.Store.GetRocketById(id)
 
 	if err != nil{
-		return Rocket{},nil
+		return Rocket{}, err
 	}
 
 	return rkt,nil
+}
+
+// InsertRocket - inserts a new rocket into the store
+func (s Service) InsertRocket (ctx context.Context, rkt Rocket) (Rocket,error) {
+	rkt, err := s.Store.InsertRocket(rkt)
+
+	if err != nil {
+		return Rocket{}, err
+	}
+
+	return rkt, err
+}
+
+// DeleteRocket - deletes a rocket by id from the store
+func (s Service) DeleteRocket (id string) error {
+	err := s.Store.DeleteRocket(id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
