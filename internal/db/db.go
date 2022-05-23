f5a83d7e21253d4240b718188a3c20726c32f6c8
oceballos <oceballosmi@gmail.com>
2022-05-23 02:01:01 -0500

    acceptance tests

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/axtoneIO/grpc-testing/internal/rocket"
@@ -40,14 +41,48 @@ func New() (Store, error) {
 	}, nil
 }
 
-func (s Store) GetRocket(id string) (rocket.Rocket,error){
-	return rocket.Rocket{},nil
+// GetRocket - retrieves a rocket from the database by id
+func (s Store) GetRocket(id string) (rocket.Rocket, error) {
+	var rkt rocket.Rocket
+	row := s.db.QueryRow(
+		`SELECT id, name, type FROM rockets WHERE id =$1;`,
+		id,
+	)
+	err := row.Scan(
+		&rkt.ID,
+		&rkt.Name,
+		&rkt.Type,
+	)
+	if err != nil && err.Error() != "sql: no rows in result set" {
+		log.Println(err.Error())
+		return rocket.Rocket{}, err
+	}
+
+	return rkt, nil
 }
 
+// AddRocket - Inserts a rocket into the rockets table
 func (s Store) AddRocket(rkt rocket.Rocket) (rocket.Rocket, error) {
-	return rocket.Rocket{}, nil
+	_, err := s.db.NamedQuery(
+		`INSERT INTO rockets
+		(id, name, type)
+		VALUES (:id, :name, :type)`,
+		rkt,
+	)
+	if err != nil {
+		return rocket.Rocket{}, err
+	}
+	return rocket.Rocket{
+		ID:   rkt.ID,
+		Name: rkt.Name,
+		Type: rkt.Type,
+	}, nil
 }
 
-func (s Store) DeleteRocket(id string) (string,error) {
-	return "",nil
+func (s Store) DeleteRocket(id string) (string, error) {
+	_, err := s.db.Exec(`DELETE FROM rockets WHERE id=$1`, id)
+	if err != nil {
+		return "", err
+	}
+	return "Rocket deleted successfully", nil
 }
--- internal/transport/grpc/handler.go
+++ internal/transport/grpc/handler.go
@@ -2,6 +2,8 @@ package grpc
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 	"net"
 
@@ -35,37 +37,70 @@ func (h Handler) Serve() error {
 	}
 
 	grpcServer := grpc.NewServer()
-	rkt.RegisterRocketServiceServer(grpcServer,&h)
+	rkt.RegisterRocketServiceServer(grpcServer, &h)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Printf("failed to serve %v", err)
 		return err
 	}
-	
+
 	return nil
 }
 
-func (h Handler) GetRocket(ctx context.Context, req *rkt.GetRocketRequest) (*rkt.GetRocketResponse,error){
-	log.Println("Get Rocket gRPC Endpoint Hit")
-
-	rocket, err := h.RocketService.GetRocket(ctx,req.Id)
-	if err != nil {
-		return &rkt.GetRocketResponse{},err
+func (h Handler) GetRocket(ctx context.Context, req *rkt.GetRocketRequest) (*rkt.GetRocketResponse, error) {
+	id := req.Id
+	if id == "" {
+		return &rkt.GetRocketResponse{}, errors.New("id parameter must be sent")
 	}
-	
+
+	rocket, err := h.RocketService.GetRocket(ctx, req.Id)
+	if err != nil {
+		return &rkt.GetRocketResponse{}, err
+	}
+
 	return &rkt.GetRocketResponse{
 		Rocket: &rkt.Rocket{
-			ID: 	rocket.ID,
-			Name:	rocket.Name,
-			Type: 	rocket.Type,
+			ID:   rocket.ID,
+			Name: rocket.Name,
+			Type: rocket.Type,
 		},
-	},nil
+	}, nil
 }
 
-func (h Handler) AddRocket(ctx context.Context, req *rkt.AddRocketRequest) (*rkt.AddRocketResponse,error){
-	return &rkt.AddRocketResponse{},nil
+func (h Handler) AddRocket(ctx context.Context, req *rkt.AddRocketRequest) (*rkt.AddRocketResponse, error) {
+	if req.Rocket == nil {
+		return &rkt.AddRocketResponse{},errors.New("rocket parameter must be sent")
+	}
+
+	newRkt, err := h.RocketService.AddRocket(ctx, rocket.Rocket{
+		ID:   req.Rocket.ID,
+		Name: req.Rocket.Name,
+		Type: req.Rocket.Type,
+	})
+	if err != nil {
+		return nil, errors.New("failed to insert rocket into database")
+	}
+	fmt.Println(newRkt)
+	return &rkt.AddRocketResponse{
+		Rocket: &rkt.Rocket{
+			ID:   newRkt.ID,
+			Name: newRkt.Name,
+			Type: newRkt.Type,
+		},
+	}, nil
 }
 
-func (h Handler) DeleteRocket(ctx context.Context, req *rkt.DeleteRocketRequest) (*rkt.DeleteRocketResponse,error){
-	return &rkt.DeleteRocketResponse{},nil
+func (h Handler) DeleteRocket(ctx context.Context, req *rkt.DeleteRocketRequest) (*rkt.DeleteRocketResponse, error) {
+	if req.Id == "" {
+		return &rkt.DeleteRocketResponse{}, errors.New("id parameter must be sent")
+	}
+
+	status, err := h.RocketService.DeleteRocket(ctx,req.Id)
+	if err != nil{
+		return nil, errors.New("failed to delete rocket from database")
+	}
+
+	return &rkt.DeleteRocketResponse{
+		Status: status,
+	}, nil
 }
